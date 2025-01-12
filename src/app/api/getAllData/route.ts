bdeddc40f45a1a6c3f166b40a8273bd9fbeeea11
Anthony Nguyen <anthonyanthonyanthonynguyen@gmail.com>
2025-01-11 20:55:57 -0800

    get and post routes for user , got rid of id as string and turned into objid for mongo

--- src/app/api/getAllData/route.ts
+++ src/app/api/getAllData/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server';
-import { run } from '../../../utils/mongodb2';
+import connect from '../../../utils/mongodb2';
 
 export async function GET() {
     try {
-        await run();
-        return NextResponse.json({ message: 'Successfully connected to MongoDB!' });
+        const client = await connect;
+        const cursor = await client.db("test").collection("users").find();
+        const greetings = await cursor.toArray();
+        return Response.json(greetings);
     } catch (error) {
         console.error(error);
         return NextResponse.json({ error: 'Failed to connect to MongoDB' }, { status: 500 });
--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,28 +1,64 @@
 import {NextRequest, NextResponse} from 'next/server';
+import { ObjectId } from "mongodb";
 import { connectMongo } from '@/utils/mongodb';
 import { UserModel } from '@/models/models';
 
+
 export async function GET(
         req: NextRequest,
-        { params }: { params: { id: string } }
+        { params }: { params: Promise<{ id: string }> }
     ) 
+
 {
     try {
+        const id = (await params).id;
+        const objId = new ObjectId(id);
+
         await connectMongo();   
 
-        if (!params.id) {
-            return NextResponse.json(
-                { error: "account ID is required" },
-                { status: 400 }
-            );
-        }
-
-        const user = await UserModel.find({id: params.id});
+        const user = await UserModel.findById({objId});
         console.log(user);
         return NextResponse.json({user});
     }
     catch {
         console.error("Trouble fetching user");
         return NextResponse.json( {error: "bruh"}, {status: 500} );
+    }
+}
+
+// TODO: this is for updating votes, but its exploitable clientside bc u could send infinite votes w/o vote 
+// tracking for each user
+export async function POST(req: NextRequest,
+                          {params} : { params: Promise<{id: string}>  }
+                          ) {
+    try {
+        const body = await req.json();
+        if (!(typeof body === "object" &&
+            typeof body.vote === "boolean")) {
+            return NextResponse.json(
+                { error: "Invalid request body" },
+                { status: 400 }
+            );
+        }
+
+        const objId = new ObjectId((await params).id);
+        
+        await connectMongo();
+
+        if (body.vote) {
+            const updatedUser = await UserModel.findOneAndUpdate({_id: objId}, {$inc: {votes: 1}});   
+            console.log(updatedUser);
+            return NextResponse.json( {updatedUser} );
+        }
+        else {
+            const updatedUser = await UserModel.findOneAndUpdate({_id: objId}, {$inc: {votes: -1}});   
+            return NextResponse.json( {updatedUser} );
+        }
+        return NextResponse.json( {error: "peepeepoopoo"}, {status: 500})
+    }
+    catch (error) {
+        console.error("Trouble updating user profile")
+        console.log(error);
+        return NextResponse.json( {error: "RAAAH"}, {status: 500})
     }
 }
